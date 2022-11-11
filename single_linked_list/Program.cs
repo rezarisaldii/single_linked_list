22ef1493c46cac8370b1a1dc1bad6565675571f5
RezaRisaldi <reza.risaldi.ft21@mail.umy.ac.id>
2022-11-11 17:18:13 +0700

    public void addNote

--- single_linked_list/Program.cs
+++ single_linked_list/Program.cs
@@ -21,6 +21,19 @@ namespace single_linked_list
         {
             START = null;
         }
+        public void addNote() // Add a node in the list
+        {
+            int nim;
+            string nm;
+            Console.Write("\nEnter the roll number of the studentt : ");
+            nim = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nEnter the name of the student : ");
+            nm = Console.ReadLine();
+            Node newnode = new Node();
+            newnode.rollNumber = nim;
+            newnode.name = nm;
+            //if the node to be inserted is the first nnode
+        }
     }
     internal class Program
     {
